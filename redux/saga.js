088b6b73ee1754e0bb797e134bb9afb8e2094f26
Rémy Greinhofer <remy.greinhofer@gmail.com>
2019-03-28 20:46:14 -0500

    Add date filtering
    Adds logic to the saga to filter the dates it retrieves. As we won't have
    any API at first, we will simply retrieve the full data set when the
    page loads for the first time (it will be cached by the service worker
    after that), and filter out the information we don't want to display.
    
    Fixes scrapd/scrapdviz#27

--- redux/saga.js
+++ redux/saga.js
@@ -1,7 +1,29 @@
+import moment from 'moment';
 import { all, delay, put, takeLatest } from 'redux-saga/effects'
 import { actionTypes, fetchData } from './store'
 import { fatalities } from '../mock-api/mock-data'
 
+function isInRange(date, from_, to) {
+  const d = moment(date);
+  if (d.isBefore(from_)) {
+    return false;
+  }
+  if (d.isAfter(to)) {
+    return false;
+  }
+  return true;
+}
+
+function filterFatalities(fatalities, from_, to) {
+  let data = new Array()
+  for (var i = 0; i < fatalities.length; i++) {
+    const d = fatalities[i]['Date'];
+    if (isInRange(d, from_, to)) {
+      data.push(fatalities[i]);
+    }
+  }
+  return data;
+}
 
 function* watchFetchDataAsync() {
   yield takeLatest(actionTypes.FETCH_DATA_ASYNC, workerFetchDataAsync);
@@ -9,8 +31,9 @@ function* watchFetchDataAsync() {
 
 function* workerFetchDataAsync(action) {
   const { from_, to } = action.payload.date_filter;
+  const f = filterFatalities(fatalities, from_, to);
   yield delay(1000)
-  yield put(fetchData(fatalities));
+  yield put(fetchData(f));
 }
 
 // Single entry point to start all Sagas at once.
