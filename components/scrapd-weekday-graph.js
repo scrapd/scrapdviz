12adc2b28ab6523a3338937298261509df25a219
Rémy Greinhofer <remy.greinhofer@gmail.com>
2019-05-07 17:46:05 -0500

    Add a weekday distribution graph
    Adds a graph showing the fatalities for each day of the week.
    
    Fixes scrapd/scrapdviz#84

---
+++ components/scrapd-weekday-graph.js
@@ -0,0 +1,38 @@
+import Proptypes from 'prop-types';
+import ScrapdGenericPieChart from './scrapd-generic-pie-chart';
+import moment from 'moment';
+import { connect } from 'react-redux';
+
+const ScrapdWeekdayGraph = props => {
+  // The data should come directly from the API.
+  let datamap = new Object();
+  datamap['Monday'] = 0;
+  datamap['Tuesday'] = 0;
+  datamap['Wednesday'] = 0;
+  datamap['Thursday'] = 0;
+  datamap['Friday'] = 0;
+  datamap['Saturday'] = 0;
+  datamap['Sunday'] = 0;
+
+  for (var i = 0; i < props.fatalities.length; i++) {
+    const date = props.fatalities[i]['Date'];
+    const dow = moment(date, ['MM/DD/YYYY']).isoWeekday();
+    const daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    const dayOfWeek = daysOfWeek[dow - 1];
+    datamap[dayOfWeek] = datamap[dayOfWeek] + 1;
+  }
+
+  return <ScrapdGenericPieChart groupedItems={datamap} itemCount={props.fatalities.length} attribute="Weekday" />;
+};
+
+ScrapdWeekdayGraph.propTypes = {
+  fatalities: Proptypes.array,
+  attribute: Proptypes.string
+};
+
+const mapStateToProps = state => {
+  const { fatalities } = state;
+  return { fatalities };
+};
+
+export default connect(mapStateToProps)(ScrapdWeekdayGraph);
