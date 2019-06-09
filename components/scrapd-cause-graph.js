25e793935e33b5e10b324201c15370f8606324d0
Rémy Greinhofer <remy.greinhofer@gmail.com>
2019-06-09 00:03:46 -0500

    Crash causes are not mutually exclusive (#110)
    Crash causes can actually be combined (i.e. someone can be speeding and
    drunk at the same time).

--- components/scrapd-cause-graph.js
+++ components/scrapd-cause-graph.js
@@ -3,29 +3,40 @@ import ScrapdGenericPieChart from './scrapd-generic-pie-chart';
 import { connect } from 'react-redux';
 
 const ScrapdCauseGraph = props => {
-  // The data should come directly from the API.
   let datamap = new Object();
   datamap['Impairment'] = 0;
   datamap['Ran light/stop'] = 0;
   datamap['Speeding'] = 0;
   datamap['Other'] = 0;
+  let total = 0;
 
   for (var i = 0; i < props.fatalities.length; i++) {
     const impairment = props.fatalities[i]['Impairment'];
     const ranLightOrStop = props.fatalities[i]['Ran light/stop'];
     const speeding = props.fatalities[i]['Speeding'];
-    if (impairment && impairment != 'none') {
+    const impaired = impairment && impairment != 'none';
+    const failedToStop = ranLightOrStop && ranLightOrStop != 'n';
+    const sped = speeding && speeding != 'n';
+
+    if (impaired) {
       datamap['Impairment'] = datamap['Impairment'] + 1;
-    } else if (ranLightOrStop && ranLightOrStop != 'n') {
+      total = total + 1;
+    }
+    if (failedToStop) {
       datamap['Ran light/stop'] = datamap['Ran light/stop'] + 1;
-    } else if (speeding && speeding != 'n') {
+      total = total + 1;
+    }
+    if (sped) {
       datamap['Speeding'] = datamap['Speeding'] + 1;
-    } else {
+      total = total + 1;
+    }
+    if (!impaired && !failedToStop && !sped) {
       datamap['Other'] = datamap['Other'] + 1;
+      total = total + 1;
     }
   }
 
-  return <ScrapdGenericPieChart groupedItems={datamap} itemCount={props.fatalities.length} attribute="Cause" />;
+  return <ScrapdGenericPieChart groupedItems={datamap} itemCount={total} attribute="Cause" />;
 };
 
 ScrapdCauseGraph.propTypes = {
