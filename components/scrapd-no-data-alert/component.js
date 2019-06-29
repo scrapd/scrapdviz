9269d1febacbb170dc44b436a8bd25b0a8f507b0
Rémy Greinhofer <remy.greinhofer@gmail.com>
2019-06-29 14:58:26 -0500

    Writes a better warning message (#140)
    Clarifies the warning message mentioning that there is no data to show
    for the selected dates.
    
    Fixes scrapd/scrapdviz#122

--- components/scrapd-no-data-alert/component.js
+++ components/scrapd-no-data-alert/component.js
@@ -1,6 +1,12 @@
 import { Alert } from 'antd';
 
 const ScrdapdNoDataAlert = ({ count }) =>
-  count == 0 && <Alert message="There is no data within the dates you selected." type="warning" showIcon />;
+  count == 0 && (
+    <Alert
+      message="There is no data within the dates you selected. Adjust the dates in the date picker above."
+      type="warning"
+      showIcon
+    />
+  );
 
 export default ScrdapdNoDataAlert;
