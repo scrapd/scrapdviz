74ed972c1304305a068830ffbdcf4552659da0e8
Jeeth Suresh <jeethsuresh@Jeeths-MacBook-Air.local>
2019-08-28 02:08:26 -0500

    Fixing lint issues

--- pages/developers.js
+++ pages/developers.js
@@ -1,11 +1,10 @@
-import ScrapdControlsContainer from '../components/scrapd-controls/container';
 import ScrapdLayout from '../components/scrapd-layout';
 import ScrapdApdViewContainer from '../components/scrapd-developers-view/container';
 import { connect } from 'react-redux';
 
 const Developers = () => (
   <ScrapdLayout>
-      <ScrapdApdViewContainer />
+    <ScrapdApdViewContainer />
   </ScrapdLayout>
 );
 
