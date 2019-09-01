eaa86b938de3147009ec6c15ce79e1ecc8030ac8
Abhra Biswas <abiswas.personal@gmail.com>
2019-08-31 23:10:15 -0500

    Delete unused JS objects
    Deletes JS objects that were in Jeeth's commit that are no longer being used. This allowed the project to pass CI tests

--- components/scrapd-hero/component.js
+++ components/scrapd-hero/component.js
@@ -1,13 +1,14 @@
+import PropTypes from 'prop-types';
 
 const heroImage = {
   height: '500px',
-    'background-image': 'url("https://res.cloudinary.com/ab91/image/upload/v1567305230/scrAPD%20Viz/hero_image.png")',
-    'background-size': 'cover',
+  'background-image': 'url("https://res.cloudinary.com/ab91/image/upload/v1567305230/scrAPD%20Viz/hero_image.png")',
+  'background-size': 'cover'
 };
 
 const text = {
   position: 'relative',
-    top: '40%'
+  top: '40%'
 };
 
 const titleStyle = {
@@ -15,15 +16,15 @@ const titleStyle = {
   width: 'auto',
   'text-align': 'center',
   color: 'white',
-    'font-weight': 'bold',
-    'margin-bottom': '0px',
+  'font-weight': 'bold',
+  'margin-bottom': '0px'
 };
 
 const subtitleStyle = {
   'font-size': '3vh',
   'text-align': 'center',
-    color: 'white',
-    'font-weight': 'bold',
+  color: 'white',
+  'font-weight': 'bold'
 };
 
 const ScrapdHero = props => {
@@ -31,12 +32,18 @@ const ScrapdHero = props => {
     <div>
       <div style={heroImage}>
         <div style={text}>
-            <h1 style={titleStyle}>{props.title}</h1>
+          <h1 style={titleStyle}>{props.title}</h1>
           <h3 style={subtitleStyle}>{props.subtitle1}</h3>
           <h3 style={subtitleStyle}>{props.subtitle2}</h3>
         </div>
       </div>
     </div>
   );
+};
+
+ScrapdHero.propTypes = {
+  title: PropTypes.string,
+  subtitle1: PropTypes.string,
+  subtitle2: PropTypes.string
 };
 export default ScrapdHero;
--- components/scrapd-home-view/component.js
+++ components/scrapd-home-view/component.js
@@ -1,40 +1,6 @@
 import ScrapdHomeSection from '../scrapd-home-section/component';
 import ScrapdHero from '../scrapd-hero/component.js';
 
-const headerStyle = {
-  height: '80vh',
-  'background-image': 'url("https://wallpaperstudio10.com/static/wpdb/wallpapers/1920x1080/177348.jpg")'
-};
-
-const titleStyle = {
-  'background-color': '#1890ff',
-  'font-size': '10vh',
-  width: 'auto',
-  'text-align': 'center'
-};
-
-const positionStyle = {
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -10%)',
-  position: 'absolute',
-  width: '100%'
-};
-const subtitleStyle = {
-  ...titleStyle,
-  'font-size': '3vh',
-  padding: '5px'
-};
-
-const smallSubtitleStyle = {
-  ...subtitleStyle,
-  'font-size': '2.5vh'
-};
-
-const hstyle = {
-  'text-align': 'center'
-};
-
 const ScrapdHomeView = () => {
   const intro = [
     <p key="0">
@@ -77,7 +43,11 @@ const ScrapdHomeView = () => {
 
   return (
     <div>
-        <ScrapdHero title="scrAPD" subtitle1="Safety data, collected automatically for you" subtitle2="ScrAPD scrapes the web for traffic fatality data, making it accessible to all"/>
+      <ScrapdHero
+        title="scrAPD"
+        subtitle1="Safety data, collected automatically for you"
+        subtitle2="ScrAPD scrapes the web for traffic fatality data, making it accessible to all"
+      />
       <ScrapdHomeSection
         title="What is ScrAPD?"
         text={intro}
