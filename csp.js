ececd172a338454cdc90df148d46838f8ca5ccb4
Rémy Greinhofer <remy.greinhofer@gmail.com>
2020-01-23 15:44:25 -0600

    Improves the security headers
    Attempts to fix all the security issues reported by the Mozilla
    Observatory: https://observatory.mozilla.org/analyze/viz.scrapd.org
    
    Fixes scrapd/scrapdviz#194

--- csp.js
+++ csp.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const helmet = require('helmet');
 const uuidv4 = require('uuid/v4');
 
@@ -5,6 +6,14 @@ const uuidv4 = require('uuid/v4');
 // https://pyvideo.org/pybay-2019/browser-security-with-http-headers.html
 // export default function csp(app) {
 module.exports = function csp(app) {
+=======
+import helmet from 'helmet';
+import uuidv4 from 'uuid/v4';
+
+// Configuration values mostly come from this talk:
+// https://pyvideo.org/pybay-2019/browser-security-with-http-headers.html
+export default function csp(app) {
+>>>>>>> Improves the security headers
   // Create a nonce on every request and make it available to other middleware
   app.use((req, res, next) => {
     res.locals.nonce = Buffer.from(uuidv4()).toString('base64');
