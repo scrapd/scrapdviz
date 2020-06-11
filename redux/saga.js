0417fca1c1a8050fac4741141e2d4eb8cbc2b542
Rémy Greinhofer <remy.greinhofer@gmail.com>
2020-06-11 08:38:25 -0500

    Fetch archive from ADP crash report (#207)
    ADP has a new data set for the crash report which gets updated
    frequently (bi-weekly?). Instead of waiting for the APD data set to be
    updated (which happens yearly), this patch uses the new data set.
    
    Drive-by:
    * Updates dependencies.

--- redux/saga.js
+++ redux/saga.js
@@ -41,6 +41,55 @@ function flattenFatalities(fatalities) {
   return datamap;
 }
 
+function adp2scrapd(fatalities) {
+  let datamap = new Array();
+  for (var i = 0; i < fatalities.length; i++) {
+    if (!fatalities[i]["atd_mode_category_metadata"]) {
+      console.log(`Invalid data for crash ${fatalities[i]["crash_id"]}`);
+      continue;
+    }
+    let ATDMetadata = JSON.parse(fatalities[i]["atd_mode_category_metadata"]);
+    for (var j = 0; j < ATDMetadata.length; j++) {
+      if (ATDMetadata[j].death_cnt === 0) {
+        console.log(`Skipping non fatal entry in crash ${fatalities[i]["crash_id"]}`);
+        continue;
+      }
+      let data = Object();
+      switch (ATDMetadata[j]["mode_id"]) {
+        case 1:
+        case 2:
+        case 4:
+          data["type"] = "motor vehicle";
+          break;
+        case 3:
+          data["type"] = "motorcycle";
+          break;
+        case 5:
+          data["type"] = "bicycle";
+          break;
+        case 6:
+          data["type"] = "micro-mobility";
+          break;
+        case 7:
+          data["type"] = "pedestrian";
+          break;
+        default:
+          data["type"] = "undefined";
+          break;
+      }
+
+      data["case"] = fatalities[i]["crash_id"];
+      data["date"] = fatalities[i]["crash_date"];
+      data["latitude"] = fatalities[i]["latitude"];
+      data["longitude"] = fatalities[i]["longitude"];
+      data["time"] = fatalities[i]["crash_time"];
+      datamap.push(data);
+    }
+  }
+
+  return datamap;
+}
+
 function filterFatalities(fatalities, from_, to) {
   let data = new Array()
   for (var i = 0; i < fatalities.length; i++) {
@@ -73,10 +122,10 @@ function* watchFetchArchivesAsync() {
 
 function* workerFetchArchivesAsync(action) {
   const { from_, to } = action.payload.date_filter;
-  const endpoint = 'https://raw.githubusercontent.com/scrapd/datasets/master/datasets/archives-all.json';
+  const endpoint = `https://data.austintexas.gov/resource/y2wy-tgr5.json?crash_fatal_fl=Y&$where=crash_date between '${moment(from_).format("YYYY-MM-DDT00:00:00.000")}' and '${moment(to).format("YYYY-MM-DDT00:00:00.000")}'`
   const response = yield call(fetch, endpoint);
   const fatalities = yield call([response, "json"]);
-  const f = filterFatalities(fatalities, from_, to);
+  const f = adp2scrapd(fatalities)
   // yield delay(1000)
   yield put(fetchArchives(f));
 }
